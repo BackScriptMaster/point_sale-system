2c50b4f8d4838bafbd6e231f07fc289e0071439c
Diego Gutierrez <JDQDeveloper@outlook.com>
2024-07-16 17:07:34 -0400

    Added fields to clients model

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    code=models.CharField(max_length=200, unique=True, blank=True)
+    name=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    phone=models.IntegerField(max_length=8)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        managed = True
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
+    
+    def __str__(self) -> str:
+        return self.code
