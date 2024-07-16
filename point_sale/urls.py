4cf4858091fb361741802753e978f8022872aa5c
Diego Gutierrez <JDQDeveloper@outlook.com>
2024-07-16 17:07:34 -0400

    Added URL for products in urls.py

--- point_sale/urls.py
+++ point_sale/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sales/', include('sales.urls')),
-    path('clients/', include('clients.urls'))
+    path('clients/', include('clients.urls')),
+    path('products/', include('products.urls'))
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
